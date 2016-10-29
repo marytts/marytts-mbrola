92c87495bd204f727ef448855c706da32be2401c
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-10-30 00:29:25 +0200

    initial logic to unpack and test MBROLA binary

--- src/main/groovy/marytts/mbrola/MbrolaSynthesis.groovy
+++ src/main/groovy/marytts/mbrola/MbrolaSynthesis.groovy
@@ -1,11 +1,17 @@
 package marytts.mbrola
 
+import groovy.util.logging.Log4j
+
 import marytts.modules.InternalModule
+import marytts.modules.synthesis.WaveformSynthesizer
 
 import static marytts.mbrola.MbrolaDataType.MBROLA
 import static marytts.datatypes.MaryDataType.AUDIO
 
+@Log4j
 class MbrolaSynthesis extends InternalModule {
+
+    WaveformSynthesizer synthesizer
 
     MbrolaSynthesis() {
         this(Locale.US)
@@ -13,5 +19,10 @@ class MbrolaSynthesis extends InternalModule {
 
     MbrolaSynthesis(Locale locale) {
         super(getClass().name, MBROLA, AUDIO, locale)
+        synthesizer = new MbrolaSynthesizer()
+        log.debug 'starting up MbrolaSynthesizer'
+        synthesizer.startup()
+        synthesizer.powerOnSelfTest()
+        log.debug 'MbrolaSynthesizer ready'
     }
 }
--- src/main/groovy/marytts/mbrola/MbrolaSynthesizer.groovy
+++ src/main/groovy/marytts/mbrola/MbrolaSynthesizer.groovy
@@ -1,5 +1,7 @@
 package marytts.mbrola
 
+import groovy.util.logging.Log4j
+
 import marytts.exceptions.SynthesisException
 import marytts.modules.synthesis.*
 
@@ -7,16 +9,26 @@ import org.w3c.dom.Element
 
 import javax.sound.sampled.AudioInputStream
 
+@Log4j
 class MbrolaSynthesizer implements WaveformSynthesizer {
+
+    File mbrola
 
     @Override
     void startup() throws Exception {
-
+        mbrola = File.createTempFile('mbrola-', '.bin')
+        mbrola.executable = true
+        def binStream = getClass().getResourceAsStream('bin/mbrola-mac')
+        mbrola << binStream.bytes
+        log.debug "unpacked MBROLA binary to: $mbrola"
     }
 
     @Override
     void powerOnSelfTest() throws Error {
-
+        def proc = "$mbrola".execute()
+        proc.waitFor()
+        def out = proc.inputStream.text
+        log.debug "\n$out"
     }
 
     @Override
